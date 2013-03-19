91e6bc647725f88e20fae58e6e007446694778f7
Xavier Duran <xdurana@uoc.edu>
2013-03-19 09:10:59 +0100

    rails new balaula --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BALAULA::Application.config.secret_token = '0ac942fdcc26e49bb348cd656767926d16730fad481a508187a8867412234fd320d72fd5f8b5d79292dafc6ae97e78733e232bf31d918fccfd23e2beaf9cd3a5'
